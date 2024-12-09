358514afbbbb6a0eee7646a91b60d284a81c7c61
Fireand <indradamar123@gmail.com>
2024-12-09 22:13:07 +0700

    first commit

---
+++ portfolio_website/bolts/project-bolt-sb1-dzhvunry/project/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#8B5CF6',
+          dark: '#7C3AED',
+        }
+      },
+    },
+  },
+  plugins: [],
+}
